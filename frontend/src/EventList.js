d5b807a3c410841d34ec0a0d44daf3163e4626cb
Carlson Sharpless <cwsharpless@gmail.com>
2021-03-30 21:09:40 -0400

    Changed event list and date format

--- frontend/src/EventList.js
+++ frontend/src/EventList.js
@@ -4,33 +4,44 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-//
+// EventList - a list of events.
 const EventList = ({events, keyword}) => {
-
-  // 
+  // Establish variables.
   const [input, setInput] = useState('');
   const [eventList, seteventList] = useState(events);
 
-  // update search bar input
+  // Update the search bar input.
   const updateSearch = async (input) => {
-    //  
+    // Get the list, filtered by type.
     const filteredByType = events.filter(event => {
-      //
-      return event.type.toLowerCase().includes(input.toLowerCase())
-    })
-      
-    //console.log("input: " + input);
-    //console.log("filteredByType: " + filteredByType);
+      return event.type.toLowerCase().includes(input.toLowerCase())})
     
-    //
+    // Set the event list to contain the filtered elements.
     setInput(input);
     seteventList(filteredByType);
+  }
+
+  // toReadableDT - Convert the MongoDB date string to something more readable.
+  const toReadableDT = (mongoDate) => {
+    // Months in English abbreviations.
+    const monthAbbrs = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    // Split into the date and time.
+    var dateTime = mongoDate.split("T");
+
+    // Split the date into year, month, and day.
+    var dateParts = dateTime[0].split("-");
+
+    // Parse the month and day and make a string of them.
+    return monthAbbrs[parseInt(dateParts[1]) - 1] + " " + parseInt(dateParts[2]);
+
+    //2021-03-31T01:01:04.789Z
   }
 
   // set the css styling for search bar
   const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
   
-  // 
+  // Create the event list.
   return (
     <div className="event-list">
       <input 
@@ -46,12 +57,12 @@ const EventList = ({events, keyword}) => {
           {console.log(event._id)}
           <Link to={`/note/${event._id}`}>
             <h2>{ event.title }</h2>
-            <p>Date: { event.date }</p>
+            <p>Date: { toReadableDT(event.date) }</p>
             <p>Location: { event.location }</p>
-            <p>Type: { event.type }</p>
+            {/*<p>Type: { event.type }</p>
             <p>Description: { event.description }</p>
             <p>RSO: { event.isRSO }</p>
-            <p>Event ID: { event.event_id }</p>
+            <p>Event ID: { event.event_id }</p>*/}
           </Link>
         </div>
       ))}
