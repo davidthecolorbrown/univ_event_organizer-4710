fd04fa09d0aab8492b6e2efc51cd999f5702cf1c
Carlson Sharpless <cwsharpless@gmail.com>
2021-04-19 15:03:36 -0400

    Added logout page

---
+++ frontend/src/Logout.js
@@ -0,0 +1,22 @@
+import { useHistory } from "react-router-dom";
+import Cookies from 'universal-cookie';
+
+// Register - A page that allows the registration of users.
+const Logout = () => {
+  // Get the history and cookie object.
+  const history = useHistory();
+  const cookies = new Cookies();
+
+  // Delete the cookie and go home.
+  cookies.remove('user');
+  history.push('/');
+
+  // Return the page.
+  return (
+    <div className="logout">
+      <h2>Logging out...</h2>
+    </div>
+  );
+}
+
+export default Logout;
