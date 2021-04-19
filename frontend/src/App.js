309f908581fbd1a0cb8530c6256208bdf64d4464
Carlson Sharpless <cwsharpless@gmail.com>
2021-04-19 14:40:46 -0400

    Added comment editor (unfinished)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AddComment from './AddComment';
 import Create from './Create';
+import EditComment from './EditComment';
 import EventDetails from './EventDetails';
 import EventView from './EventView';
 import Login from './Login';
@@ -27,6 +28,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/comment/edit/:id">
+              <EditComment/> 
             </Route>
             <Route path="/comment/:id">
               <AddComment/> 
