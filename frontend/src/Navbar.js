3d4a60f8a4d5a649c96fb1eeaac44e9a34fe188a
Carlson Sharpless <cwsharpless@gmail.com>
2021-04-18 22:02:39 -0400

    Navbar affected by login status
    Sorta.

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Cookies from 'universal-cookie';
 
 const Navbar = () => {
+  // Get the cookie object.
+  const cookies = new Cookies();
+  const [userCookie, setUserCookie] = useState(cookies.get('user'));
+
+  // FIXME: Make this run only when cookie is changed.
+  const refreshCookie = () => {
+    setUserCookie(cookies.get('user'));
+  }
+
   return (
     <nav className="navbar">
       <h1>Event Manager</h1>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#000000',
-          borderRadius: '8px' 
-        }}>New Event</Link>
-      </div>
+      {userCookie === undefined &&
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/Register">Register</Link>
+          <Link to="/Login" style={{ 
+            color: 'white', 
+            backgroundColor: '#000000',
+            borderRadius: '8px' 
+          }}>Login</Link>
+        </div>}
+      {userCookie !== undefined &&
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#000000',
+            borderRadius: '8px' 
+          }}>New Event</Link>
+        </div>}
     </nav>
   );
 }
